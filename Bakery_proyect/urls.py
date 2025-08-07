14ac6e5262b8428ed70dcddd103a339cfdec26c4
smartinezg2018 <simonmartinez1820@gmail.com>
2025-08-07 17:16:16 -0500

    creating models in inventory, making the html so all products are listed

--- Bakery_proyect/urls.py
+++ Bakery_proyect/urls.py
@@ -19,8 +19,15 @@ from django.urls import path
 
 from inventory import views as viewsInventory
 from products import views as viewsProduct
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inventory/',viewsInventory.inventory),
     path('products/',viewsProduct.product),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
