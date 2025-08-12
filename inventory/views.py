4c7c4238da9d97a0f9573817e664662cb4312427
Alejandra Suarez <mayerlysuarez1508@gmail.com>
2025-08-12 15:16:09 -0500

    Inventory with raw materials only

--- inventory/views.py
+++ inventory/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-from .models import Product, RawMaterial
+from .models import RawMaterial
 
 def inventory(request):
-    productos = Product.objects.all()
     materias_primas = RawMaterial.objects.all()
     return render(request, 'inventory/inventory.html', {
-        'productos': productos,
         'materias_primas': materias_primas
     })
