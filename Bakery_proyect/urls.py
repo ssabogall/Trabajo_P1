a1d10ea955faf6df31ead4945bb27b83d45afeef
Santiago Sabogal Lozano <ssabogall@eafit.edu.co>
2025-08-13 18:33:00 -0500

    Clean up: Remove unnecessary blank lines in Bakery_proyect/urls.py
    - Improved code formatting and readability
    - No functional changes, only style cleanup

--- Bakery_proyect/urls.py
+++ Bakery_proyect/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from inventory import views as viewsInventory
 from products import views as viewsProduct
-
 from django.conf import settings
 from django.conf.urls.static import static
 
