bb7fbb3cb168d811cfb1e99131e98f8fa32d462a
smartinezg2018 <simonmartinez1820@gmail.com>
2025-09-01 15:29:36 -0500

    ad to db

--- cvs_to_json.py
+++ cvs_to_json.py
@@ -1,13 +1,20 @@
 import pandas as pd
 import json
+import sys
 
-# Lee el archivo CSV
-df = pd.read_csv('products.csv')
+if len(sys.argv) < 2:
+    print("Usage: python run.py <csv_file>")
+    sys.exit(1)
 
-# Guarda el DataFrame como JSON
-df.to_json('products.json', orient='records')
+csv_file = sys.argv[1]
 
-with open('products.json', 'r') as file:
+print(f"Using file: {csv_file}")
+
+df = pd.read_csv(csv_file)
+
+df.to_json(f'{csv_file[:-4]}.json', orient='records')
+
+with open(f'{csv_file[:-4]}.json', 'r') as file:
     movies = json.load(file)
 
 for i in range(100):
